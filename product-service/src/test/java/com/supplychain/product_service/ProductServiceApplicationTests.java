7860edb71334236b16d5e10045b04515d6298561
antosubin <antosubin66@gmail.com>
2024-12-30 05:01:44 +0530

    First Commit Product-service

---
+++ product-service/src/test/java/com/supplychain/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
